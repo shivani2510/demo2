bdc9c40a5b3f767191d1659fa27ed0daea985efa
shivani2510 <shivani.gajjar25@gmail.com>
2017-09-15 19:48:36 +0530

    default

---
+++ src/app/layout/base/default-base.component.ts
@@ -0,0 +1,10 @@
+/****** Created by shivani gajjar(shivani.gajjar@kahunasystems.com) 14/9/17 11:09 AM*******/
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'base',
+  templateUrl:'default-base.html'
+})
+export class DefaultBaseComponent{
+
+}
